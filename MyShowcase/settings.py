b30a7e14ffc8e791f8238d4d91f531eb718b0320
UltimateVimal <vimal.catchme@gmail.com>
2022-01-29 20:37:32 +0530

    updated dettings

--- MyShowcase/settings.py
+++ MyShowcase/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 EMAIL_USE_TLS=EMAIL_USE_TLS
 EMAIL_HOST=EMAIL_HOST
 EMAIL_HOST_USER=EMAIL_HOST_USER
-EMAIL_HOST_PASSWORD=config('EMAIL_HOST_PASSWORD',default='')
+EMAIL_HOST_PASSWORD=config('EMAIL_HOST_PASSWORD',default='',cast=str)
 EMAIL_PORT=EMAIL_PORT
 
 # Quick-start development settings - unsuitable for production
